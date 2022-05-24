d52cb8767295df0b0f350fc0228e208ff5751c1b
ladudasha <darya.gameza@gmail.com>
2022-05-24 23:12:30 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+
+// 005
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// }
+
+// obj.a = 10
+// console.log(obj);
+
+
+// 006
+
+// let number = 4;
+// console.log(4/0);
+// console.log('string' * 9);
+
+
+// const persone = 'Alex';
+// const person = '5';
+
+// const bool = true;
+
+// console.log(something);
+
+// const obj = {
+//      name: 'john',
+//      age: 25,
+//      isMarried: false
+// }
+// console.log(obj.name);
+
+
+//             0           1            2   3          4   5          
+// let arr = ['plam.png', 'orange.jpg', 6, 'apple.bmp', {}, []]
+
+// 007
+
+// alert('hello')
+
+// const result = confirm('are you here');
+// console.log(result);
+
+const answer = prompt('Вам есть 18', '18');
+console.log(answer);
