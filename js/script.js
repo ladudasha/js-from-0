6cb75f89d04425209337fdf708ef1b11f53b51af
ladudasha <darya.gameza@gmail.com>
2022-05-26 23:29:00 +0300

    007 008 009

--- js/script.js
+++ js/script.js
@@ -49,5 +49,50 @@
 // const result = confirm('are you here');
 // console.log(result);
 
-const answer = prompt('Вам есть 18', '18');
-console.log(answer);
+// const answer = +prompt('Вам есть 18', '18');
+// console.log (typeof(answer));
+
+
+
+
+// const answers = []
+// answers[0] = prompt('Как ваше имя?','')
+// answers[1] = prompt('Как ваше фамилия?','')
+// answers[3] = prompt('Сколько вам лет?','')
+
+// document.write(answers);
+// console.log (typeof(answers));
+
+
+
+// const category = 'toys';
+
+// console.log(`https//someurl.com/${category}/5` );
+
+// const user = 'Darya'
+// alert(`Hello, ${user}`);
+
+// console.log('arr' + " - object");
+
+// let incr = 10;
+// let decr = 10;
+
+// incr++
+// decr--
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+console.log(2 + 2 * 2 != '6');
+// console.log(4+4 == '8');
+// console.log(4+4 === '8');
+
+// const isChecked = false;
+// const isClose = true;
+
+// console.log(isChecked || !isClose)
+
+
+
+
